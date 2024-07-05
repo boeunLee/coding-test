212931e51232a8cbadd77753837bf4581ec2f5c0
boeunLee <bounjaeun@gmail.com>
2024-07-06 01:15:16 +0900

    실버1-숨바꼭질 수정

--- BFS/백준 실버/실버1-숨바꼭질.js
+++ BFS/백준 실버/실버1-숨바꼭질.js
@@ -4,33 +4,23 @@ const K = 17;
 function bfs() {
   let queue = [];
   let visited = Array(100001).fill(0);
-  let min = Number.MAX_SAFE_INTEGER;
-  queue.push(N);
-  let end = false;
-  while (!end) {
-    let n = queue.shift();
+  visited[N] = 1;
+  queue.push([N,1]);
+  while (true) {
+    // time을 넣으면 계산하기 더 편함
+    let [n,time] = queue.shift();
 
     for (x of [n - 1, n + 1, 2 * n]) {
       if (x === K) {
-        end = true;
-        break;
-      }else {
-        queue.push(x);
-        visited[x] = visited[n] + 1;
+        return time;
+      } else {
+        if (visited[x] === 0) {
+          queue.push([x, time+1]);
+          visited[x] = 1;
+        }
       }
     }
-    // for (let i = 0; i < visited.length; i++){
-    //   if (visited[i] > 0) {
-    //     console.log(i, visited[i]);
-    //   }
-    // }
   }
-  for (let i = 0; i < visited.length; i++){
-    if (visited[i] > 0) {
-      min = Math.min(min, visited[i]);
-    }
-  }
-  console.log(min+1)
 }
 
-bfs();
+console.log(bfs());
